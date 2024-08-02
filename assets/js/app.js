1b2a2199bd47932eaace472dfe652281bc8ca0dc
mohitgoyat525 <montyits71@gmail.com>
2024-08-02 09:10:31 +0530

    modify-slick-dots

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,3 +34,37 @@ $(".slick-slider-parent").slick({
     // instead of a settings object
   ],
 });
+
+$(".second-slider-box").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+
+  ],
+});
